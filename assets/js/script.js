521b48c5af460963ed934df60b39005bc2f3772d
alexberg90 <ayazya@code.edu.az>
2023-02-26 12:58:13 +0400

    js-dom

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+let btn=document.getElementById("addBtn");
+let input=document.getElementById("textInput");
+let list = document.getElementById("itemList");
+let removeBtn=document.getElementById("removeBtn")
+
+
+btn.addEventListener("click",function(){
+    let value=input.value;
+    if (value=="") {
+        alert("can not be empty");
+        return
+
+    }
+
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    span.innerHTML = "--X--";
+    
+    span.onclick = function () {
+        this.parentElement.remove();
+    };
+    
+       
+    li.classList.add("list-group-item","hover");
+    li.style.cursor="pointer";
+    li.innerText=value.trim();
+    li.appendChild(span);
+    list.appendChild(li);
+    input.value = "";
+});
+
+
